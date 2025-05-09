8f42a806281b20555bf990a2f25ee371ca5a1e3f
OptimusOpus <taylor@labrys.io>
2025-05-09 14:29:03 +1000

    fix: update glue-mainnet and solana contract configurations, enhance send task logging

--- tasks/evm/send.ts
+++ tasks/evm/send.ts
@@ -7,6 +7,8 @@ import { makeBytes32 } from '@layerzerolabs/devtools'
 import { EndpointId } from '@layerzerolabs/lz-definitions'
 
 import { getLayerZeroScanLink } from '../solana'
+import { formatEther, formatUnits } from 'ethers/lib/utils'
+import { Options } from '@layerzerolabs/lz-v2-utilities'
 
 interface TaskArguments {
     dstEid: number
@@ -32,20 +34,31 @@ const action: ActionType<TaskArguments> = async (
     // }
 
     const amountLD = BigNumber.from(amount)
+
+    const options = Options.newOptions().addExecutorLzReceiveOption(200000, 0).toHex().toString()
+
     const sendParam = {
         dstEid,
         to: makeBytes32(bs58.decode(to)),
         amountLD: amountLD.toString(),
         minAmountLD: amountLD.mul(9_000).div(10_000).toString(),
-        extraOptions: '0x',
+        extraOptions: options,
         composeMsg: '0x',
         oftCmd: '0x',
     }
     const [msgFee] = await token.functions.quoteSend(sendParam, false)
 
+    console.log('native fee:', formatEther(msgFee.nativeFee))
+    const balance = await signer.provider.getBalance(signer.address)
+    console.log('balance:', formatEther(balance))
+
+    const tokenBalance = await token.balanceOf(signer.address)
+    const decimals = await token.decimals()
+    console.log('token balance:', formatUnits(tokenBalance, decimals))
+
     const txResponse = await token.functions.send(sendParam, msgFee, signer.address, {
         value: msgFee.nativeFee,
-        gasLimit: 500_000,
+        // gasLimit: 500_000,
     })
     const txReceipt = await txResponse.wait()
     console.log(`send: ${amount} to ${to}: ${txReceipt.transactionHash}`)
